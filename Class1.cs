58e9887aaa97bfcca30a588713e899ecbe1a250d
DIM945001 <dmitrij1630626@gmail.com>
2021-06-06 13:20:24 +0400

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        private double p; // периметр
+
+        
+        
+        public double A; // открытый вариант переменной "а" для передачи значения в закрытую переменную 
+          
+        private double a
+        {
+            get { return A; }
+            set { a = A; }
+        }
+        
+        
+        public double B; // открытый вариант переменной "b" для передачи значения в закрытую переменную 
+        private double b
+        {
+            get { return B; }
+            set { b = B; }
+        }
+        
+
+        public double C; // открытый вариант переменной "c" для передачи значения в закрытую переменную 
+        private double c
+        {
+            get { return C; }
+            set { c = C; }
+        }
+
+        public double S; // переменная, которая принимает результат и выводит значение на экран
+
+
+
+
+
+        public void math() //Все операции выполнялись над закрытыми переменными
+        {
+            p = a + b + c; // нахождение периметра
+            S = Math.Sqrt(p/2 * (p/2 - a) * (p/2 - b) * (p/2 - c)); // сама формула площади
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1(); // объявляю экземплляр класса
+            Console.Write("введите А: "); 
+            
+            c1.A = double.Parse(Console.ReadLine()); // ввод значения переменной А и передача значеня в закрытую переменную
+            Console.Write("введите В: ");
+            c1.B = double.Parse(Console.ReadLine()); // ввод значения переменной А и передача значеня в закрытую переменную
+            Console.Write("введите С: ");
+            c1.C = double.Parse(Console.ReadLine());// ввод значения переменной А и передача значеня в закрытую переменную
+            c1.math(); // Запускаю метод в котором прописаны операции для вычисления значения переменных "p" и "s"
+            Console.WriteLine("Площадь равна: {0}",c1.S); // вывод резульата
+            Console.ReadKey(); // точка выхода из программы
+        }
+
+    }
+}
